56c17a73c915225d527841f174ec445f10ba7e33
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2023-12-12 09:13:53 +0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -5,28 +5,22 @@ from dotenv import load_dotenv
 
 
 
-load_dotenv()
-bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
-bisal_channel = "https://telegram.me/bisal_files"
-bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
-
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '24081336'))
-    API_HASH = str(getenv('API_HASH', 'ca055a6b7f2ed170ddafb8bfdb8a3523'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6874744511:AAGQS_x-OMGVxT3314NPasqlzCu_xrqAqxQ'))
-    name = str(getenv('name', 'Chatgpt_adv_bot'))
+    API_ID = int(getenv('API_ID', '21845036'))
+    API_HASH = str(getenv('API_HASH', 'b9787357754c57417854ba8ca0d35129'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6825240097:AAF3cdpLPj0ATx9Uo7gWH0oemP95wiAIZTQ'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001943068381'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001933661618'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001991880018'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001991880018'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5397984467").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5510849897").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'biisal'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'riplinker'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -39,8 +33,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://filestrm:filestrm@cluster0.ukc8hwb.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'bisal_files'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779 -1001393593437 -1002031180571")).split()))   
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://BIGBOSS:BIGBOSS@cluster0.ii3gmvr.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'hdlinks4uu'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002031180571")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1001734958816")).split()))   
     
