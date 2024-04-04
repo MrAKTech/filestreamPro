5e54464a3c3446c1fe03a64603c92f2a73b421fc
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-04-04 15:05:35 +0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '21845036'))
     API_HASH = str(getenv('API_HASH', 'b9787357754c57417854ba8ca0d35129'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6687536584:AAGpD_E4ET8r_cBlOpBVRDhCgaT2368GYEk'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6687536584:AAHkfH69rkTXB91WSubKz9KHrMCZEPCa3P0'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     name = str(getenv('name', 'Chatgpt_adv_bot'))
     WORKERS = int(getenv('WORKERS', '4'))
