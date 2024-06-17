85c007b2ae2864dae1c278334f86380c33207e25
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2024-06-17 09:31:42 +0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,8 +16,8 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     name = str(getenv('name', 'Chatgpt_adv_bot'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002119311683'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002119311683'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001953315183'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001953315183'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
